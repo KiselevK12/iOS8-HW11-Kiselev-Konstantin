9ec0c19fdfc4809e6e81af0be434c044e3a8ce21
Konstantin <kostikem165@gmail.com>
2022-12-30 13:24:56 +0300

    Add comment structure ViewController

--- iOS8-HW11-Kiselev Konstantin/Sources/ViewController.swift
+++ iOS8-HW11-Kiselev Konstantin/Sources/ViewController.swift
@@ -9,12 +9,24 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - UI Elements
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    // MARK: - LifeCycle
+    
+    // MARK: - Setup
+    
+    private func setupView() {
+        view.backgroundColor = .systemCyan
+    }
+    
+    // MARK: - Action
 
 }
 
+// MARK: - Extensions
